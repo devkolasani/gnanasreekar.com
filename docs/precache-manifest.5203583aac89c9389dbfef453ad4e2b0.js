0505226b43ca85eef40b7cb18fe9c1aee463746d
Krishna Vivek Kolasani <vivekkolasani45@gmail.com>
2020-07-01 11:22:10 +0530

    V 0.1.2
    Made build structure compatible with github

---
+++ docs/precache-manifest.5203583aac89c9389dbfef453ad4e2b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5079310287e38b89709e0e8cf132943c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3e1970d198c6e431998",
+    "url": "/static/css/main.0376093f.chunk.css"
+  },
+  {
+    "revision": "e9bff42d14346e64e3b2",
+    "url": "/static/js/2.75044210.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.75044210.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3e1970d198c6e431998",
+    "url": "/static/js/main.802cfadd.chunk.js"
+  },
+  {
+    "revision": "9e984364485af53b156a",
+    "url": "/static/js/runtime-main.8eefb073.js"
+  }
+]);
