9ac7e5e51a0eff59d07f031f5ce9708280d32174
Krishna Vivek Kolasani <vivekkolasani45@gmail.com>
2020-07-01 12:01:23 +0530

    Updates

---
+++ precache-manifest.881006604082a87e0f8fba727cc645cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "955e9a32103b80e8f9a29282d4bd8ff2",
+    "url": "/gnanasreekar.com/index.html"
+  },
+  {
+    "revision": "c527dd1ff972255f7882",
+    "url": "/gnanasreekar.com/static/css/main.0376093f.chunk.css"
+  },
+  {
+    "revision": "d839de950c69601f6c25",
+    "url": "/gnanasreekar.com/static/js/2.b4dfd0d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gnanasreekar.com/static/js/2.b4dfd0d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c527dd1ff972255f7882",
+    "url": "/gnanasreekar.com/static/js/main.1d6e7a78.chunk.js"
+  },
+  {
+    "revision": "5974bbf3d585bbd52d50",
+    "url": "/gnanasreekar.com/static/js/runtime-main.d09d677e.js"
+  }
+]);
