23c00939911ba73e91fac40e9a1e69b7dab4bba4
Krishna Vivek Kolasani <vivekkolasani45@gmail.com>
2020-07-01 12:47:37 +0530

    V 0.1.6
    Minor Updates

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 
 function App() {
-    return <div className="App">Under Constructor</div>;
+    return <div className="App">Under Construction</div>;
 }
 
 export default App;
