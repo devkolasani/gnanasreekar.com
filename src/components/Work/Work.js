c37a3d0e177ce4a500e30d69a3c144d7023bc462
Krishna Vivek Kolasani <vivekkolasani45@gmail.com>
2020-09-04 10:44:18 +0530

    V 0.5.1
    Desktop Work Animation Smoothing

--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -1,8 +1,9 @@
 import React, { useContext } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 
 // Styles And Modules
-import WorkImages from "./WorkImages";
 import styles from "./Work.module.scss";
+import WorkImages from "./WorkImages";
 import { WorkContext } from "../../contexts/WorkContext";
 
 const Work = () => {
@@ -17,60 +18,74 @@ const Work = () => {
             <div className={styles.TitleBar}></div>
           </div>
         </div>
-        <div className={styles.Details}>
-          <p className={styles.Name}>{work["name"]}</p>
-          <p className={styles.Description}>{work["description"]}</p>
-          <div className={styles.Links}>
-            <div className={styles.Link}>
-              <p>{work["linkname"]}</p>
-              <a target="_blank" rel="noopener noreferrer" href={work["link"]}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="30.621"
-                  height="30.621"
-                  viewBox="0 0 30.621 30.621"
+        <AnimatePresence exitBeforeEnter>
+          <motion.div
+            className={styles.Details}
+            exit={{ opacity: 0, y: 10 }}
+            initial={{ opacity: 0, y: 10 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8 }}
+          >
+            <motion.p className={styles.Name}>{work["name"]}</motion.p>
+            <motion.p className={styles.Description}>
+              {work["description"]}
+            </motion.p>
+            <motion.div className={styles.Links}>
+              <div className={styles.Link}>
+                <p>{work["linkname"]}</p>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href={work["link"]}
                 >
-                  <g
-                    id="Icon_feather-external-link"
-                    works-name="Icon feather-external-link"
-                    transform="translate(-3 -2.379)"
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="30.621"
+                    height="30.621"
+                    viewBox="0 0 30.621 30.621"
                   >
-                    <path
-                      id="Path_153"
-                      works-name="Path 153"
-                      d="M27,19.5v9a3,3,0,0,1-3,3H7.5a3,3,0,0,1-3-3V12a3,3,0,0,1,3-3h9"
-                      fill="none"
-                      stroke="#bcbabc"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="3"
-                    />
-                    <path
-                      id="Path_154"
-                      works-name="Path 154"
-                      d="M22.5,4.5h9v9"
-                      fill="none"
-                      stroke="#bcbabc"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="3"
-                    />
-                    <path
-                      id="Path_155"
-                      works-name="Path 155"
-                      d="M15,21,31.5,4.5"
-                      fill="none"
-                      stroke="#bcbabc"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="3"
-                    />
-                  </g>
-                </svg>
-              </a>
-            </div>
-          </div>
-        </div>
+                    <g
+                      id="Icon_feather-external-link"
+                      works-name="Icon feather-external-link"
+                      transform="translate(-3 -2.379)"
+                    >
+                      <path
+                        id="Path_153"
+                        works-name="Path 153"
+                        d="M27,19.5v9a3,3,0,0,1-3,3H7.5a3,3,0,0,1-3-3V12a3,3,0,0,1,3-3h9"
+                        fill="none"
+                        stroke="#bcbabc"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="3"
+                      />
+                      <path
+                        id="Path_154"
+                        works-name="Path 154"
+                        d="M22.5,4.5h9v9"
+                        fill="none"
+                        stroke="#bcbabc"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="3"
+                      />
+                      <path
+                        id="Path_155"
+                        works-name="Path 155"
+                        d="M15,21,31.5,4.5"
+                        fill="none"
+                        stroke="#bcbabc"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="3"
+                      />
+                    </g>
+                  </svg>
+                </a>
+              </div>
+            </motion.div>
+          </motion.div>
+        </AnimatePresence>
         <WorkImages images={work["images"]} />
         <div className={styles.Navigation}>
           <p className={styles.NavIndicator}>{work["id"]}</p>
