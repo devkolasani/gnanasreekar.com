8e633d07919f306f8cb671e520883f514faa75ce
Krishna Vivek Kolasani <vivekkolasani45@gmail.com>
2020-09-06 20:26:49 +0530

    Updates

---
+++ precache-manifest.3e7c77666a50bedd8495e5f52f2e481a.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c58d6d21789dd26f4f1d9cc7c5f9019",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b500720ca3b80258747c",
+    "url": "/static/css/main.a4f2b4d5.chunk.css"
+  },
+  {
+    "revision": "58a3b2a1fdd878af7dfb",
+    "url": "/static/js/2.420d6623.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.420d6623.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b500720ca3b80258747c",
+    "url": "/static/js/main.9b1f742d.chunk.js"
+  },
+  {
+    "revision": "db6cf0a04b1f7088d974",
+    "url": "/static/js/runtime-main.5067620d.js"
+  },
+  {
+    "revision": "4e4732cbea80a25297cb0fad86f215c1",
+    "url": "/static/media/CEMS1.4e4732cb.png"
+  },
+  {
+    "revision": "972e85d276a5042c0396fd8e0e2127fc",
+    "url": "/static/media/CEMS2.972e85d2.png"
+  },
+  {
+    "revision": "34e5a29b67653dea2e88b502f248fa13",
+    "url": "/static/media/CEMS3.34e5a29b.png"
+  },
+  {
+    "revision": "34e5a29b67653dea2e88b502f248fa13",
+    "url": "/static/media/COVID1.34e5a29b.png"
+  },
+  {
+    "revision": "7709773e8dce6ed72c117df28a963f23",
+    "url": "/static/media/COVID2.7709773e.png"
+  },
+  {
+    "revision": "232724e786d903038cb766c67d87484f",
+    "url": "/static/media/COVID3.232724e7.png"
+  },
+  {
+    "revision": "093acec89693703793bfef947d2d53fb",
+    "url": "/static/media/GothamPro-Bold.093acec8.woff"
+  },
+  {
+    "revision": "1d847f40bd9cc27d146689cbe64d1635",
+    "url": "/static/media/GothamPro-Bold.1d847f40.ttf"
+  },
+  {
+    "revision": "2d2bd9710e826385e20810ef9152c6fb",
+    "url": "/static/media/GothamPro-Bold.2d2bd971.woff2"
+  },
+  {
+    "revision": "66216243619355b4920bb3e960c282f7",
+    "url": "/static/media/GothamPro-Bold.66216243.svg"
+  },
+  {
+    "revision": "69b0a7e4ac89380251a80522b753dabf",
+    "url": "/static/media/GothamPro-Bold.69b0a7e4.eot"
+  },
+  {
+    "revision": "50238f48a2bf1f2889ca4ee5b08a4cf0",
+    "url": "/static/media/MRMS1.50238f48.png"
+  },
+  {
+    "revision": "ef446664f8b924bdef5c530dd9a3494e",
+    "url": "/static/media/MRMS2.ef446664.png"
+  },
+  {
+    "revision": "e7757ad190b5cbcd68d6d52d88f11ee3",
+    "url": "/static/media/MRMS3.e7757ad1.png"
+  },
+  {
+    "revision": "9ead9edc200c1287574aa8764a78d0f8",
+    "url": "/static/media/OS1.9ead9edc.png"
+  },
+  {
+    "revision": "8bd55494e41eff5a499526c85387b1e0",
+    "url": "/static/media/OS2.8bd55494.png"
+  },
+  {
+    "revision": "db442c4e517048dd981d08bbc2614d55",
+    "url": "/static/media/OS3.db442c4e.png"
+  },
+  {
+    "revision": "486724d848a43a1225d7458e034a463f",
+    "url": "/static/media/Resume.486724d8.pdf"
+  },
+  {
+    "revision": "9d3328d52d4d1a7e25f841e434cecba7",
+    "url": "/static/media/navclosed.9d3328d5.svg"
+  },
+  {
+    "revision": "b363812274c448c0bf6bb025191a6d66",
+    "url": "/static/media/navopenalt.b3638122.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.891cbd770c3016551967a37167433e97.js"
+  "/precache-manifest.3e7c77666a50bedd8495e5f52f2e481a.js"
 );
 
 self.addEventListener('message', (event) => {
