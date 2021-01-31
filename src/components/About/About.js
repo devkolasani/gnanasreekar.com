1091cdf90567ff623c5533dbe9c896945b6df1cf
Krishna Vivek Kolasani <vivekkolasani45@gmail.com>
2021-01-31 08:16:39 +0530

    V 1.0.1

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,43 +6,42 @@ import Resume from "../../assets/Resume.pdf";
 import styles from "./About.module.scss";
 
 const About = () => {
-  let desc = `Hello, I'm Gnana Sreekar. How's it going?\n\
+	let desc = `Hello, I'm Gnana Sreekar. How's it going?\n\
 I'm an Android developer based in India and a hardware geek. \
 I love working on IoT projects like automating my home.\
 I'm a tech enthusiast, have a YouTube channel that focuses on all things tech.`;
 
-  return (
-    <div className={styles.About} id="About">
-      <div className={styles.Container}>
-        <div className={styles.Title}>
-          <div className={styles.TitleContent}>
-            <p>About</p>
-            <div className={styles.TitleBar}></div>
-          </div>
-        </div>
-        <div className={styles.Description}>
-          <p>{desc}</p>
-        </div>
-        <div className={styles.DescriptionMobile}>
-          <p>
-            Hello, I'm Gnana Sreekar. How's it going? I'm an Android developer
-            based in India and a hardware geek.
-          </p>
-        </div>
-        <div className={styles.Download}>
-          <a
-            href={Resume}
-            target="_blank"
-            download="Resume.pdf"
-            rel="noopener noreferrer"
-          >
-            Download Resume
-          </a>
-        </div>
-        <Illustration />
-      </div>
-    </div>
-  );
+	return (
+		<div className={styles.About} id="About">
+			<div className={styles.Container}>
+				<div className={styles.Title}>
+					<div className={styles.TitleContent}>
+						<p>About</p>
+						<div className={styles.TitleBar}></div>
+					</div>
+				</div>
+				<div className={styles.Description}>
+					<p>{desc}</p>
+				</div>
+				<div className={styles.DescriptionMobile}>
+					<p>
+						Hello, I'm Gnana Sreekar. How's it going? I'm an Android
+						developer based in India and a hardware geek.
+					</p>
+				</div>
+				<div className={styles.Download}>
+					<a
+						href="https://drive.google.com/file/d/1aaVYPMN5CdxaUyjCWf1iYI2swWGgcKxR/view"
+						rel="noopener noreferrer"
+						target="_blank"
+					>
+						Download Resume
+					</a>
+				</div>
+				<Illustration />
+			</div>
+		</div>
+	);
 };
 
 export default About;
--- src/contexts/WorkContext.js
+++ src/contexts/WorkContext.js
@@ -18,69 +18,70 @@ import COVID3 from "../assets/images/COVID3.png";
 export const WorkContext = createContext();
 
 const WorkContextProvider = (props) => {
-  const workstore = [
-    {
-      id: "01",
-      name: "MRMS",
-      description:
-        "MRMS(Learning Management System) is an android app that helps you keep track of everything happening at school by teachers and students as well as parents.",
-      linkname: "Play Store",
-      link: "https://play.google.com/store/apps/details?id=com.rgs.mrms",
-      images: [MRMS1, MRMS2, MRMS3],
-    },
-    {
-      id: "02",
-      name: "CEMS",
-      description:
-        "CEMS(Campus Electricity Management System) is an android app that helps you track electricity usage on campus with analytics.",
-      linkname: "GitHub",
-      link: "https://github.com/gnanasreekar/CEMS",
-      images: [CEMS1, CEMS2, CEMS3],
-    },
-    {
-      id: "03",
-      name: "COVID-19",
-      description:
-        "COVID-19 is an android app that helps you keep track of current cases in India with statewise analytics.",
-      linkname: "GitHub",
-      link: "https://github.com/gnanasreekar/Covid-19",
-      images: [COVID1, COVID2, COVID3],
-    },
-    {
-      id: "04",
-      name: "OneSignal Client",
-      description:
-        "OneSignal Client to send Push notifications from Mobile With OneSignal API",
-      linkname: "play Store",
-      link: "https://play.google.com/store/apps/details?id=com.rgs.onesignal",
-      images: [OSC1, OSC2, OSC3],
-    },
-  ];
+	const workstore = [
+		{
+			id: "01",
+			name: "MRMS",
+			description:
+				"MRMS(Learning Management System) is an android app that helps you keep track of everything happening at school by teachers and students as well as parents.",
+			linkname: "Play Store",
+			link: "https://play.google.com/store/apps/developer?id=R+G+S",
+			images: [MRMS1, MRMS2, MRMS3],
+		},
+		{
+			id: "02",
+			name: "CEMS",
+			description:
+				"CEMS(Campus Electricity Management System) is an android app that helps you track electricity usage on campus with analytics.",
+			linkname: "GitHub",
+			link: "https://github.com/gnanasreekar/CEMS",
+			images: [CEMS1, CEMS2, CEMS3],
+		},
+		{
+			id: "03",
+			name: "COVID-19",
+			description:
+				"COVID-19 is an android app that helps you keep track of current cases in India with statewise analytics.",
+			linkname: "GitHub",
+			link: "https://github.com/gnanasreekar/Covid-19",
+			images: [COVID1, COVID2, COVID3],
+		},
+		{
+			id: "04",
+			name: "OneSignal Client",
+			description:
+				"OneSignal Client to send Push notifications from Mobile With OneSignal API",
+			linkname: "play Store",
+			link:
+				"https://play.google.com/store/apps/details?id=com.sreekar.onesignal",
+			images: [OSC1, OSC2, OSC3],
+		},
+	];
 
-  // States
-  const [works] = useState(workstore);
-  const [index, setIndex] = useState(0);
-  const [work, setWork] = useState(workstore[index]);
+	// States
+	const [works] = useState(workstore);
+	const [index, setIndex] = useState(0);
+	const [work, setWork] = useState(workstore[index]);
 
-  const prevWork = () => {
-    if (index === 0) {
-      setIndex(works.length - 1);
-    } else {
-      setIndex(index - 1);
-    }
-    setWork(works[index]);
-  };
+	const prevWork = () => {
+		if (index === 0) {
+			setIndex(works.length - 1);
+		} else {
+			setIndex(index - 1);
+		}
+		setWork(works[index]);
+	};
 
-  const nextWork = () => {
-    setIndex((index + 1) % works.length);
-    setWork(works[index]);
-  };
+	const nextWork = () => {
+		setIndex((index + 1) % works.length);
+		setWork(works[index]);
+	};
 
-  return (
-    <WorkContext.Provider value={{ work, prevWork, nextWork }}>
-      {props.children}
-    </WorkContext.Provider>
-  );
+	return (
+		<WorkContext.Provider value={{ work, prevWork, nextWork }}>
+			{props.children}
+		</WorkContext.Provider>
+	);
 };
 
 export default WorkContextProvider;
